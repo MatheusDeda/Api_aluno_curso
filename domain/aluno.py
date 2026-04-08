c895f7ed9d730d08d8af3c57e69885e692ecd7c8
Matheus Deda De Oliveira <matheus.oliveira@parquetec.se>
2026-04-08 11:18:24 -0300

    Atualização Completa Api

--- domain/aluno.py
+++ domain/aluno.py
@@ -1,22 +1,8 @@
-import uuid #gera automaticamente
+import uuid
 from dataclasses import dataclass, field
-from typing import Optional
-from enum import Enum
 
 @dataclass
 class Aluno:
     nome: str
     email: str
     id: str = field(default_factory=lambda: str(uuid.uuid4()), init=False)
-
-    def __init__(self, nome: str, email: str, id: str):
-        self.nome = nome
-        self.email = email
-        self.id = id
-
-        print(f"Objeto Aluno criado para: {self.nome}")
-        print(f"Objeto email criado para: {self.email}")
-        
-
-novo_aluno = Aluno (nome="matheus", email="matheus@gmail.com", id= "")
-print(novo_aluno)
--- services/aluno_service.py
+++ services/aluno_service.py
@@ -2,11 +2,12 @@ from domain.aluno import Aluno
 from repositories.aluno_repository import aluno_repo
 
 def criar_aluno(nome: str, email: str):
-    
     if aluno_repo.buscar_por_email(email):
         raise ValueError("Já existe um aluno cadastrado com este e-mail.")
-        
+    
+    # Aqui criamos o objeto. O ID é gerado pelo molde (domain) automaticamente.
     novo_aluno = Aluno(nome=nome, email=email)
+    
     return aluno_repo.salvar(novo_aluno)
 
 def listar_alunos():
